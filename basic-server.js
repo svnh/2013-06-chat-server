54ad862a6bc65927e697173db5cc777701fe37fa
Hack Reactor Students <github@hackreactor.com>
2013-07-09 16:03:33 -0700

    exports+require

--- basic-server.js
+++ basic-server.js
@@ -1,25 +1,9 @@
 var http = require("http");
-
-var requestListener = function (request, response) {
-  console.log("Serving request type " + request.method + " for url " + request.url);
-  var statusCode = 200;
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = "text/plain";
-  response.writeHead(statusCode, headers);
-  response.end("Hello, World!");
-};
-
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
-};
+var requestListener = require("./request-handler");
 
 var port = 8080;
-
 var ip = "127.0.0.1";
 
-var server = http.createServer(requestListener);
+var server = http.createServer(requestListener.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
--- request-handler.js
+++ request-handler.js
@@ -4,6 +4,21 @@
  * from this file and include it in basic-server.js. Check out the
  * node module documentation at http://nodejs.org/api/modules.html. */
 
-var handleRequest = function(request, response) {
-
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
 };
+
+var handleRequest = function(request, response) {
+  console.log("Serving request type " + request.method + " for url " + request.url);
+  var statusCode = 200;
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = "text/plain";
+  response.writeHead(statusCode, headers);
+  response.end("Hello, World!");
+};
+
+
+exports.handleRequest = handleRequest;
