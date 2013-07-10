6d86e3494d5337ce8ecd6c8efff3d81845653aa7
Hack Reactor Students <github@hackreactor.com>
2013-07-09 21:26:01 -0700

    only 12 JSON's, success

--- request-handler.js
+++ request-handler.js
@@ -6,8 +6,7 @@
 var url = require('url');
 var messages = []; //todo: handle multiple rooms
 
-var parseMsgObj = function (JSONmessage) {
-  message = JSON.parse(JSONmessage);
+var parseMsgObj = function (message) {
   var result = {
     "username": message.username || "anon",
     "createdAt": new Date(),
@@ -16,7 +15,6 @@ var parseMsgObj = function (JSONmessage) {
   return result;
 };
 
-var tempX = JSON.stringify({"results":[{"text":" hello","createdAt":"2013-07-09T19:05:28.916Z","updatedAt":"2013-07-09T19:05:28.916Z","objectId":"RhhvKuJMga"},{"text":" Messageadsfdf","createdAt":"2013-07-09T19:04:51.494Z","updatedAt":"2013-07-09T19:04:51.494Z","objectId":"B1UaarZgWa"},{"username":"Chief Keef","text":"Where all the spambot shawties at?","createdAt":"2013-07-09T17:59:07.357Z","updatedAt":"2013-07-09T17:59:07.357Z","objectId":"uS73zEY9wI"},{"username":"Chief Keef","text":"Where all the spambot shawties at?","createdAt":"2013-07-09T17:59:06.280Z","updatedAt":"2013-07-09T17:59:06.280Z"}]});
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -46,34 +44,27 @@ var handleRequest = function(req, res) {
 
     // route requests
     if (req.method === "GET") {
-      // console.log(JSON.stringify(messages));
-      // handleResponse(200, JSON.stringify(messages), 'text');
       var temZ = JSON.stringify({"results": messages });
       console.log(temZ);
-      handleResponse(200, temZ, 'text');
+      handleResponse(200, temZ, 'test');
 
     } else if (req.method === "POST") {
 
       var result = "";
 
       // we start receiving data
-      req.on("data", function(message) {
-        result += JSON.stringify(parseMsgObj(message));
+      req.on("data", function(chunk) {
+        result += chunk;
       });
 
       // we finish receiving data
       req.on("end", function(){
-        messages.push(result);
-        temp = {
-          "result": result
-        };
+        messages.push(parseMsgObj(JSON.parse(result)));
         handleResponse(201, "message recieved", 'text');
-        // handleResponse(200, JSON.stringify(temp), 'JSON');
       });
     }
   }
 
-  // console.log("Serving request type " + req.method + " for url " + req.url);
   handleResponse(200, "awesome job", 'text');
 };
 
